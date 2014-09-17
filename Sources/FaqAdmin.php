b6971e4b7c5f38c4e86b4184e5764a13ad727868
Suki <suki@missallsunday.com>
2014-09-17 11:01:21 -0500

    Create yet another file, there is just too many subactions to handle :(
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/FaqAdmin.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @package FAQ mod
+ * @version 2.1
+ * @author Jessica González <suki@missallsunday.com>
+ * @copyright Copyright (c) 2014, Jessica González
+ * @license https://www.mozilla.org/MPL/2.0/
+ */
+
+if (!defined('SMF'))
+	die('No direct access');
+
+class FaqAdmin extends FaqTools
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
