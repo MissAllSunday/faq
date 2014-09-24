3462abab025f992404955a3ed70665b3f801f5e6
Suki <suki@missallsunday.com>
2014-09-24 18:27:47 -0500

    I suppose it makes sense to move all hooks here...
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/FaqAdmin.php
+++ Sources/FaqAdmin.php
@@ -116,4 +116,61 @@ class FaqAdmin extends FaqTools
 			'faq_per_classic',
 			'faq_per_simple');
 	}
+
+
+	function menu(&$menu_buttons)
+	{
+		global $scripturl, $modSettings, $txt, $context;
+
+		$insert = $this->enable('menuPosition') ? $this->setting('menuPosition') : 'home';
+		$counter = 0;
+
+		foreach ($menu_buttons as $area => $dummy)
+			if (++$counter && $area == $insert)
+				break;
+
+		$menu_buttons = array_merge(
+			array_slice($menu_buttons, 0, $counter),
+			array('faq' => array(
+				'title' => $this->text('title'),
+				'href' => $scripturl . '?action=faq',
+				'show' => $this->enable('enable') && allowedTo('faq_view') ? true : false,
+				'sub_buttons' => array(
+					'faq_admin' => array(
+						'title' => $this->text('manageFaqs'),
+						'href' => $scripturl . '?action=faq;sa=manage',
+						'show' => allowedTo('faq_edit'),
+						'sub_buttons' => array(
+							'faq_add' => array(
+								'title' => $this->text('addNew'),
+								'href' => $scripturl . '?action=faq;sa=add',
+								'show' => allowedTo('faq_add'),
+							),
+						),
+					),
+					'faq_category' => array(
+						'title' => $this->text('manageCategories'),
+						'href' => $scripturl . '?action=faq;sa=manageCat',
+						'show' => allowedTo(array('faq_delete', 'faq_add', 'faq_edit')),
+						'sub_buttons' => array(),
+					),
+					'faq_admin_settings' => array(
+						'title' => $this->text('titleAdmin'),
+						'href' => $scripturl . '?action=admin;area=modsettings;sa=faq',
+						'show' => allowedTo('admin_forum'),
+						'sub_buttons' => array(),
+					),
+				),
+			)),
+			array_slice($menu_buttons, $counter)
+		);
+	}
+
+	function care()
+	{
+		// Pay no attention to that girl behind the curtain...
+		if ($this->enable('care'))
+			return '
+		<a href="http://missallsunday.com" target="_blank" title="Free SMF mods">FAQ mod &copy; Suki</a>';
+	}
 }
