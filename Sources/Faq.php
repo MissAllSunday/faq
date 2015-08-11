8aff70d05856dc59b6cb3b5111b7eecb59d6cdb1
Suki <suki@missallsunday.com>
2015-08-11 10:06:11 -0500

    Crazy idea! lets us declare our hooks where we really need them...
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/Faq.php
+++ Sources/Faq.php
@@ -34,10 +34,6 @@ class Faq extends FaqTools
 	// Define the hooks we are going to use.
 	protected $_availableHooks = array(
 		'actions' => 'integrate_actions',
-		'menu' => 'integrate_menu_buttons',
-		'permissions' => 'integrate_load_permissions',
-		'adminAreas' => 'integrate_admin_areas',
-		'modifications' => 'integrate_modify_modifications',
 	);
 
 	public function __construct()
